90456f8db73db47f7a026a10ae74048d63c7bf2b
Jack Carrozzo <jack@crepinc.com>
2025-02-23 01:26:26 -0500

    split graph b-ask

--- src/modulation.lisp
+++ src/modulation.lisp
@@ -485,3 +485,41 @@
                collecting x)
          fft-mags)))))
 ;; TODO: figure out why this is not just the left side ^
+
+@export
+(defun graph-ask-fft-split (&key (sample-rate 48000) (symbol-rate 3000) (num-symbols 64) (cfreq -5000))
+  (multiple-value-bind (gold-gen gold-repeat-n)
+      (cl-radar.corr::make-gold-code-generator :offset 3 :taps1 '(1 3 4 5 6) :taps2 '(6 4 2 1 0))
+    (declare (ignore gold-repeat-n))
+    (let* ((samp-per-sym (/ (float sample-rate) symbol-rate))
+           (num-samples (* num-symbols samp-per-sym))
+           (ask-mod (make-ask-modulator sample-rate symbol-rate cfreq -20.0))
+           (gold-code (make-array num-symbols :initial-contents
+                                  (funcall gold-gen num-symbols)))
+           (bb-samples (funcall ask-mod gold-code))
+           (prev-pow-2 (expt 2 (1- (ceiling (/ (float (log (length bb-samples))) (float (log 2)))))))
+           (bb-fft (bordeaux-fft:windowed-fft bb-samples (/ prev-pow-2 2)
+                                              prev-pow-2))
+           (fft-swapped (cl-radar.math:fft-swap bb-fft))
+           (fft-reals (cl-radar.math:array-mapcar #'realpart fft-swapped))
+           (fft-imags (cl-radar.math:array-mapcar #'imagpart fft-swapped)))
+      (format t "-- ~a symbols, ~a samples.~%" num-symbols num-samples)
+      (format t "----: ~a~%" (subseq bb-samples 0 6))
+      (format t "-- fft got back ~a~%" (length bb-fft))
+      (format t "---: ~a~%" (subseq bb-fft 0 6))
+
+      (let* ((fft-len (length bb-fft))
+             (f-start (* -1 (/ sample-rate 2.0)))
+             (f-end (/ sample-rate 2.0))
+             (d-f (/ sample-rate fft-len))
+             (x-axis (loop for x from f-start to f-end by d-f
+                           collecting x)))
+        (vgplot:plot
+         x-axis
+         fft-reals
+         "real"
+         x-axis
+         fft-imags
+         "imag")))))
+
+;; (cl-radar.mod::graph-ask-fft-split :symbol-rate 8000)
